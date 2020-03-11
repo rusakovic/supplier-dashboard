a78a3f32cefc4d75e8e1ca05549f1a26c6d91ff5
rusakovic <maxrusakovich@gmail.com>
2020-03-11 12:51:31 +0300

    Updates

---
+++ precache-manifest.8a4525f55b433ec9eea4f5def7d5e9bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a888d06ecca8129d7011009e2c33d0",
+    "url": "/supplier-dashboard/index.html"
+  },
+  {
+    "revision": "8ef77d0f1def59780ba6",
+    "url": "/supplier-dashboard/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4dbafff764448cb1a506",
+    "url": "/supplier-dashboard/static/css/main.4a541119.chunk.css"
+  },
+  {
+    "revision": "8ef77d0f1def59780ba6",
+    "url": "/supplier-dashboard/static/js/2.15e37fbf.chunk.js"
+  },
+  {
+    "revision": "aa762385ab423a29206547471fcc4bac",
+    "url": "/supplier-dashboard/static/js/2.15e37fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dbafff764448cb1a506",
+    "url": "/supplier-dashboard/static/js/main.e76d25b2.chunk.js"
+  },
+  {
+    "revision": "67b4863bf95d7f69361d",
+    "url": "/supplier-dashboard/static/js/runtime-main.0989caef.js"
+  }
+]);
